59563ef26b652664b2d386cc8615e39a8c29461e
SarekSJ <Syke117@gmail.com>
2017-11-12 16:23:34 -0800

    Added faculty to dummy data

--- make_dummy_data.py
+++ make_dummy_data.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import sessionmaker
 from tabledef import *
 from datetime import datetime
 
-engine = create_engine('mysql://root:root@localhost/coen174', echo=True)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
@@ -11,8 +10,9 @@ session = Session()
 
 session.add(Course('Gonzaga University',  'United States', 'CS 145', 'COEN 122', 'Rejected', 'Figueira', datetime.now(), "Not close enough to the syllabus of COEN 122"))
 session.add(Course('San Jose State University',  'United States', 'RLGN 345', 'RSOC 12', 'Approved', 'Atkinson', datetime.now(), "Covered everything in RSOC 12"))
-session.add((Course('Saint Edwards University',  'United States', 'MATH 2', 'AMTH 108', 'Rejected', 'Fang', datetime.now(), "Not good")))
-session.add(User('Zach Graham', 'Zip Dog'))
-session.add(User('Brandon Smith', 'Zip Dog'))
+session.add((Course('Saint Edwards University',  'United States', 'MATH 2', 'AMTH 108', 'Rejected', 'Fang', datetime.now(), "AMTH 108 has a completely different syllabus")))
+session.add(User('Figueira', 'Zip Dog'))
+session.add(User('Fang', 'Zip Dog'))
+session.add(User('Atkinson', 'Zip Dog'))
 
 session.commit()
