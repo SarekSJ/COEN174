a95cfe4c3c575874bb70d31d28365dd42ca47834
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2017-11-13 12:50:23 -0800

    Add files via upload

---
+++ templates/dropdown.js
@@ -0,0 +1,252 @@
+var schools = new Array();
+
+schools['Argentina'] = new Array('','school1');
+schools['Australia'] = new Array('','school1');
+schools['Austria'] = new Array('','school1');
+schools['Belgium'] = new Array('','school1');
+schools['Bhutan'] = new Array('','school1');
+schools['Botswana'] = new Array('','school1');
+schools['Cambodia'] = new Array('','school1');
+schools['Chile'] = new Array('','school1');
+schools['China'] = new Array('','school1');
+schools['Costa Rica'] = new Array('','school1');
+schools['Czech Republic'] = new Array('','school1');
+schools['Denmark'] = new Array('','school1');
+schools['Dominican Republic'] = new Array('','school1');
+schools['Ecuador'] = new Array('','school1');
+schools['El Salvador'] = new Array('','school1');
+schools['France'] = new Array('','school1');
+schools['Germany'] = new Array('','school1');
+schools['Ghana'] = new Array('','school1');
+schools['Greece'] = new Array('','school1');
+schools['Iceland'] = new Array('','school1');
+schools['India'] = new Array('','school1');
+schools['Ireland'] = new Array('','school1');
+schools['Italy'] = new Array('','school1');
+schools['Japan'] = new Array('','school1');
+schools['Morocco'] = new Array('','school1');
+schools['Netherlands'] = new Array('','school1');
+schools['New Zealand'] = new Array('','school1');
+schools['Nicaragua'] = new Array('','school1');
+schools['Panama'] = new Array('','school1');
+schools['Peru'] = new Array('','school1');
+schools['Rwanda'] = new Array('','school1');
+schools['Singapore'] = new Array('','school1');
+schools['South Africa'] = new Array('','school1');
+schools['South Korea'] = new Array('','school1');
+schools['Spain'] = new Array('','school1');
+schools['Sweden'] = new Array('','school1');
+schools['Switzerland'] = new Array('','school1');
+schools['Taiwan'] = new Array('','school1');
+schools['Tanzania'] = new Array('','school1');
+schools['Thailand'] = new Array('','school1');
+schools['Turkey'] = new Array('','school1');
+schools['United Kingdom'] = new Array('','school1');
+schools['United States of America'] = new Array('','Gonzaga University', 'Saint Edwards University', 'San Jose State University');
+schools['Vietnam'] = new Array('','school1');
+
+var courses = new Array();
+
+course['Argentina'] = new Array();
+course['Argentina'][''] = new Array('');
+course['Argentina']['school1'] = new Array('','course1','course2');
+
+
+course['Australia'] = new Array();
+course['Australia'][''] = new Array('');
+course['Australia']['school1'] = new Array('','course1','course2');
+
+course['Austria'] = new Array();
+course['Austria'][''] = new Array('');
+course['Austria']['school1'] = new Array('','course1','course2');
+
+course['Belgium'] = new Array();
+course['Belgium'][''] = new Array('');
+course['Belgium']['school1'] = new Array('','course1','course2');
+
+course['Bhutan'] = new Array();
+course['Bhutan'][''] = new Array('');
+course['Bhutan']['school1'] = new Array('','course1','course2');
+
+course['Botswana'] = new Array();
+course['Botswana'][''] = new Array('');
+course['Botswana']['school1'] = new Array('','course1','course2');
+
+course['Cambodia'] = new Array();
+course['Cambodia'][''] = new Array('');
+course['Cambodia']['school1'] = new Array('','course1','course2');
+
+course['Chile'] = new Array();
+course['Chile'][''] = new Array('');
+course['Chile']['school1'] = new Array('','course1','course2');
+
+course['China'] = new Array();
+course['China'][''] = new Array('');
+course['China']['school1'] = new Array('','course1','course2');
+
+course['Costa Rica'] = new Array();
+course['Costa Rica'][''] = new Array('');
+course['Costa Rica']['school1'] = new Array('','course1','course2');
+
+course['Czech Republic'] = new Array();
+course['Czech Republic'][''] = new Array('');
+course['Czech Republic']['school1'] = new Array('','course1','course2');
+
+course['Denmark'] = new Array();
+course['Denmark'][''] = new Array('');
+course['Denmark']['school1'] = new Array('','course1','course2');
+
+course['Dominican Republic'] = new Array();
+course['Dominican Republic'][''] = new Array('');
+course['Dominican Republic']['school1'] = new Array('','course1','course2');
+
+course['Ecuador'] = new Array();
+course['Ecuador'][''] = new Array('');
+course['Ecuador']['school1'] = new Array('','course1','course2');
+
+course['El Salvador'] = new Array();
+course['El Salvador'][''] = new Array('');
+course['El Salvador']['school1'] = new Array('','course1','course2');
+
+course['France'] = new Array();
+course['France'][''] = new Array('');
+course['France']['school1'] = new Array('','course1','course2');
+
+course['Germany'] = new Array();
+course['Germany'][''] = new Array('');
+course['Germany']['school1'] = new Array('','course1','course2');
+
+course['Ghana'] = new Array();
+course['Ghana'][''] = new Array('');
+course['Ghana']['school1'] = new Array('','course1','course2');
+
+course['Greece'] = new Array();
+course['Greece'][''] = new Array('');
+course['Greece']['school1'] = new Array('','course1','course2');
+
+course['Iceland'] = new Array();
+course['Iceland'][''] = new Array('');
+course['Iceland']['school1'] = new Array('','course1','course2');
+
+course['India'] = new Array();
+course['India'][''] = new Array('');
+course['India']['school1'] = new Array('','course1','course2');
+
+course['Ireland'] = new Array();
+course['Ireland'][''] = new Array('');
+course['Ireland']['school1'] = new Array('','course1','course2');
+
+course['Italy'] = new Array();
+course['Italy'][''] = new Array('');
+course['Italy']['school1'] = new Array('','course1','course2');
+
+course['Japan'] = new Array();
+course['Japan'][''] = new Array('');
+course['Japan']['school1'] = new Array('','course1','course2');
+
+course['Morocco'] = new Array();
+course['Morocco'][''] = new Array('');
+course['Morocco']['school1'] = new Array('','course1','course2');
+
+course['Netherlands'] = new Array();
+course['Netherlands'][''] = new Array('');
+course['Netherlands']['school1'] = new Array('','course1','course2');
+
+course['New Zealand'] = new Array();
+course['New Zealand'][''] = new Array('');
+course['New Zealand']['school1'] = new Array('','course1','course2');
+
+course['Nicaragua'] = new Array();
+course['Nicaragua'][''] = new Array('');
+course['Nicaragua']['school1'] = new Array('','course1','course2');
+
+course['Panama'] = new Array();
+course['Panama'][''] = new Array('');
+course['Panama']['school1'] = new Array('','course1','course2');
+
+course['Peru'] = new Array();
+course['Peru'][''] = new Array('');
+course['Peru']['school1'] = new Array('','course1','course2');
+
+course['Rwanda'] = new Array();
+course['Rwanda'][''] = new Array('');
+course['Rwanda']['school1'] = new Array('','course1','course2');
+
+course['Singapore'] = new Array();
+course['Singapore'][''] = new Array('');
+course['Singapore']['school1'] = new Array('','course1','course2');
+
+course['South Africa'] = new Array();
+course['South Africa'][''] = new Array('');
+course['South Africa']['school1'] = new Array('','course1','course2');
+
+course['South Korea'] = new Array();
+course['South Korea'][''] = new Array('');
+course['South Korea']['school1'] = new Array('','course1','course2');
+
+course['Spain'] = new Array();
+course['Spain'][''] = new Array('');
+course['Spain']['school1'] = new Array('','course1','course2');
+
+course['Sweden'] = new Array();
+course['Sweden'][''] = new Array('');
+course['Sweden']['school1'] = new Array('','course1','course2');
+
+course['Switzerland'] = new Array();
+course['Switzerland'][''] = new Array('');
+course['Switzerland']['school1'] = new Array('','course1','course2');
+
+course['Taiwan'] = new Array();
+course['Taiwan'][''] = new Array('');
+course['Taiwan']['school1'] = new Array('','course1','course2');
+
+course['Tanzania'] = new Array();
+course['Tanzania'][''] = new Array('');
+course['Tanzania']['school1'] = new Array('','course1','course2');
+
+course['Thailand'] = new Array();
+course['Thailand'][''] = new Array('');
+course['Thailand']['school1'] = new Array('','course1','course2');
+
+course['Turkey'] = new Array();
+course['Turkey'][''] = new Array('');
+course['Turkey']['school1'] = new Array('','course1','course2');
+
+course['United Kingdom'] = new Array();
+course['United Kingdom'][''] = new Array('');
+course['United Kingdom']['school1'] = new Array('','course1','course2');
+
+course['United States of America'] = new Array();
+course['United States of America'][''] = new Array('');
+course['United States of America']['Gonzaga University'] = new Array('','CS 145');
+course['United States of America']['Saint Edwards University'] = new Array('','MATH 2');
+course['United States of America']['San Jose State'] = new Array('','RLGN 345');
+
+course['Vietnam'] = new Array();
+course['Vietnam'][''] = new Array('');
+course['Vietnam']['school1'] = new Array('','course1','course2');
+
+function setSchool() {
+  c = document.getElementById('country');
+  schoolList = schools[c.value];
+  changeSelect('schools', schoolList, schoolList);
+  setCities();
+}
+
+function setCourses() {
+  c = document.getElementById('country');
+  s = document.getElementById('schools');
+  courseList = course[c.value][s.value];
+  changeSelect('course', courseList, courseList);
+}
+
+function changeSelect(fieldID, newOptions, newValues) {
+  selectField = document.getElementById(fieldID);
+  selectField.options.length = 0;
+  for (i=0; i<newOptions.length; i++) {
+    selectField.options[selectField.length] = new Option(newOptions[i], newValues[i]);
+  }
+}
+
+
+
