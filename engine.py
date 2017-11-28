b9db43c2f35cdfe25bab66ae34b58587febdc413
SarekSJ <Syke117@gmail.com>
2017-11-28 07:43:02 -0800

    Made engine.py a little easier to add information to

--- engine.py
+++ engine.py
@@ -1,5 +1,12 @@
 from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('<put db string here>', echo=True)
+sql_username = "<MySql_username>"
+sql_password = "<MySql_password>"
+hostname = "<MySql_Hostname>"
+port = "" # Leave empty if your MySql server isn't attached to a specific port
+database_name = "<MySql_DatabaseName>"
+
+
+engine = create_engine('mysql://'+sql_username+':'+sql_password+'@'+hostname+port+'/'+database_name, echo=True)
 Base = declarative_base()
